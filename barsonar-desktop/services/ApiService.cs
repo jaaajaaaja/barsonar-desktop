be6799c9db3312e8d6491720c25120db9a843f1a
jaaajaaaja <andkob0126@gmail.com>
2026-03-18 15:36:54 +0100

    added login to service

--- barsonar-desktop/services/ApiService.cs
+++ barsonar-desktop/services/ApiService.cs
@@ -1,5 +1,8 @@
-﻿using System.Net;
+﻿using barsonar_desktop.classes;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace barsonar_desktop.services
 {
@@ -25,6 +28,34 @@ namespace barsonar_desktop.services
             {
                 BaseAddress = new Uri(BASE_URL)
             };
+        }
+
+        public async Task<User> LoginAsync(string email, string password)
+        {
+            var response = await _httpClient.PostAsJsonAsync("/auth/login", new { email, password });
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            var res = JsonSerializer.Deserialize<JsonElement>(json);
+
+            var user = new User
+            {
+                Id = res.GetProperty("userId").GetInt32(),
+                Email = res.GetProperty("email").GetString() ?? "",
+                Role = res.GetProperty("role").GetString() ?? "user"
+            };
+
+            var me = await _httpClient.GetAsync("/auth/me");
+            me.EnsureSuccessStatusCode();
+            var profile = await me.Content.ReadFromJsonAsync<User>();
+
+            if (profile != null)
+            {
+                user.Username = profile.Username;
+                user.Age = profile.Age;
+            }
+
+            return user;
         }
     }
 }
