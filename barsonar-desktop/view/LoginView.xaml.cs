4138ad13ce4e365897b259d40444d206644833b7
jaaajaaaja <andkob0126@gmail.com>
2026-03-19 07:40:35 +0100

    added login window with style

---
+++ barsonar-desktop/view/LoginView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using barsonar_desktop.services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace barsonar_desktop.view
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void bntMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ShowError(string message)
+        {
+            txtError.Text = message;
+            txtError.Visibility = Visibility.Visible;
+        }
+    }
+}
